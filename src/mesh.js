df5cdee8bdebc9668caca1c9c97ed62b3c2823f6
Adnan Wahab <mail@adnanwahab.com>
2014-02-19 01:35:23 -0800

    add mesh.draw

--- src/mesh.js
+++ src/mesh.js
@@ -1,11 +1,23 @@
-function Mesh (bufferlist) {
+function Mesh (primitive, bufferlist) {
   this.buffers = {}
 
 }
 
 
 Mesh.prototype = {
-  draw: function () {}
+  draw: function () {
+    for (var b in this.buffers) {
+      this.buffers[b]
+      gl.bindBuffer(gl.ARRAY_BUFFER, this.buffers[b].buffer)
+      gl.vertexAttribPointer(this.buffers[b].loc, this.buffers[b].length, gl.FLOAT, false, 0, 0)
+      gl.enableVertexAttribArray(this.buffers[b].loc)
+      if (this.buffers[b].changed)
+        gl.bufferData(gl.array_buffer, this.buffers[b].array)
+    }
+
+    gl.drawArrays(this.primitive, this.offset, this.count)
+
+  }
 , set: function () {}
 , addAttr: function () {}
 , removeAttr: function () {}
