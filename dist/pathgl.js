0e61632484ca57377f192053f0b698d9eff86672
Adnan Wahab <mail@adnanwahab.com>
2014-03-03 00:08:04 -0800

    kill dead code

--- dist/pathgl.js
+++ dist/pathgl.js
@@ -554,20 +554,6 @@ function Mesh (primitive) {
 }
 ;var p1, p2, p3, p4
 
-function initBuffersp() {
-  p1 = makeBuffer()
-  p2 = makeBuffer()
-  p3 = makeBuffer()
-  p4 = makeBuffer()
-}
-
-function makeBuffer () {
-  var b = gl.createBuffer()
-  gl.bindBuffer(gl.ARRAY_BUFFER, b)
-  gl.bufferData(gl.ARRAY_BUFFER, 5e6, gl.DYNAMIC_DRAW)
-  return b
-}
-
 function drawPoints(elapsed) {
   pointMesh.draw()
 }
--- src/points.js
+++ src/points.js
@@ -1,19 +1,5 @@
 var p1, p2, p3, p4
 
-function initBuffersp() {
-  p1 = makeBuffer()
-  p2 = makeBuffer()
-  p3 = makeBuffer()
-  p4 = makeBuffer()
-}
-
-function makeBuffer () {
-  var b = gl.createBuffer()
-  gl.bindBuffer(gl.ARRAY_BUFFER, b)
-  gl.bufferData(gl.ARRAY_BUFFER, 5e6, gl.DYNAMIC_DRAW)
-  return b
-}
-
 function drawPoints(elapsed) {
   pointMesh.draw()
 }
