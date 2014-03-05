904ebad4975fef13a9bf372548a8f998edadfd9d
Adnan Wahab <mail@adnanwahab.com>
2014-03-05 02:13:27 -0800

    remove unused functions and improve clamp

--- src/util.js
+++ src/util.js
@@ -8,22 +8,13 @@ function extend (a, b) {
   return a
 }
 
-function twoEach(list, fn, gl) {
-  var l = list.length - 1, i = 0
-  while(i < l) fn.call(gl, list[i++], list[i++])
-}
 
-function isId(str) {
-  return false
-}
-
-function each(obj, fn) {
-  for (var key in obj) fn(obj[key], key, obj)
-}
 
 function hash(str) {
   return str.split("").reduce(function(a,b) { a = ((a << 5) - a) + b.charCodeAt(0); return a & a }, 0)
 }
+
+function each(obj, fn) { for (var key in obj) fn(obj[key], key, obj) }
 
 function uniq(ar) { return ar.filter(function (d, i) { return ar.indexOf(d) == i }) }
 
@@ -31,9 +22,8 @@ function push(d) { return this.push(d) }
 
 function flatten(ar) { return ar.reduce(function (a, b) { return a.concat(b.map ? flatten(b) : b) }) }
 
-function clamp (a, x) {
-  a = Math.abs(a)
-  return x < -a ? -a : x > a ? a : x
+function clamp (x, min, max) {
+  return Math.min(Math.max(x, min), max)
 }
 
 function range(a, b) {
