5606599811638bcc3f8b8c1b4cade0bc57851f00
Adnan Wahab <mail@adnanwahab.com>
2014-03-19 19:08:52 -0700

    neat

--- examples/physics.js
+++ examples/physics.js
@@ -1,7 +1,11 @@
+var particles = pathgl.sim.force(512 * 512).repeat()
+
 d3.select('canvas').selectAll("circle")
-.data(pathgl.sim.force(512 * 512).repeat().unwrap())
+.data(particles.unwrap())
 .enter().append("circle")
 .attr('r', function (d,i) { return d.x })
 .attr('cx', function (d,i) { return d.x })
 .attr('cy', function (d,i) { return d.y })
 .attr('fill', function () { return 'hsl(' + (Math.random() * 240 + 120) + ',100%, 50%)' })
+
+d3.select('canvas').on('mousemove', particles.emit)
