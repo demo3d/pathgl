db91d8ff23fa6e85da7a7a71db67c8baea5d5fac
Adnan Wahab <mail@adnanwahab.com>
2014-03-31 00:30:20 -0700

    animate color

--- examples/physics.js
+++ examples/physics.js
@@ -7,5 +7,6 @@ d3.select('canvas').selectAll("circle")
 .attr('cx', function (d,i) { return d.x })
 .attr('cy', function (d,i) { return d.y })
 .attr('fill', function () { return 'hsl(' + (Math.random() * 240 + 120) + ',100%, 50%)' })
+.shader({'stroke': 'unpack_color(stroke) + vec4(texture2D(texture, abs(pos.xy)).x, .5, texture2D(texture, abs(pos.xy)).w, .1)'})
 
 d3.select('canvas').on('click', particles.reverse)
