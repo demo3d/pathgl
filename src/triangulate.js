057d121be251657e02f727bc7b1e985100d05a06
Adnan Wahab <mail@adnanwahab.com>
2014-04-08 18:28:02 -0500

    triangulation export

--- src/triangulate.js
+++ src/triangulate.js
@@ -1,3 +1,20 @@
+function triangulate(curves) {
+  triangulator.Perp(0, 0, 1)
+  var t = []
+  triangulator.StartShape(t)
+  for (var i = 0; i < curves.length; i++) {
+    triangulator.StartCurve()
+    var curve = curves[i]
+    for (var j = 0; j < curve.length; j += 2) {
+      var xys = [curve[j], curve[j+1], 0]
+      triangulator.point(xys, xys)
+    }
+    triangulator.EndCurve()
+  }
+  triangulator.EndShape()
+  return t
+}
+
 function T() {
   this.state = T.cond.T_SLEEP
   this.perp = [0, 0, 0]
@@ -37,4 +54,56 @@ T.prototype.on = function (w, fn) {
   if (w == T.opt.SURFACE)  this.Surface_ = (fn)
 
   return this
+}
+
+T.prototype.StartCurve = function() {
+  this.requireState_(T.cond.T_IN_SHAPE)
+  this.state = T.cond.T_IN_CURVE
+  this.lastLine_ = null
+  if (this.storeCount > 0) this.emptyStore = true
+}
+
+T.prototype.EndCurve = function() {
+  this.requireState_(T.cond.T_IN_CURVE)
+  this.state = T.cond.T_IN_SHAPE
+}
+
+T.prototype.EndShape = function() {
+  this.requireState_(T.cond.T_IN_SHAPE)
+  this.state = T.cond.T_SLEEP
+
+  if (this.surface === null) {
+    if (!this.flagLine && !this.Surface_ && T.drawStore(this)) return this.shapeStat_ = null
+    this.emptyStore_()
+  }
+  T.projectShape(this)
+  T.computeInner(this)
+
+  if (!this.fatalFailure) {
+    this.lineOnly ?
+      T.FollowId(this.surface, 1, true) :
+      T.patchInner(this.surface)
+
+    this.surface.fixSurface()
+
+    if (this.Start_ || this.End_ || this.point_ ||
+        this.LineFlag_ || this.StartStat_ || this.EndStat_ ||
+        this.pointStat_ || this.LineFlagStat_) {
+
+      this.lineOnly ?
+        T.drawLine(this, this.surface) :
+        T.drawSurface(this, this.surface)
+    }
+
+    if (this.Surface_) {
+      T.discardOuter(this.surface)
+      this.Surface_(this.surface)
+      this.surface = null
+      return this.shapeStat_ = null
+    }
+  }
+
+  T.surface.killSurface(this.surface)
+  this.shapeStat_ = null
+  this.surface = null
 }
