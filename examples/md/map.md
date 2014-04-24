1991eeac646676d2e180f372bbc54c1b3467976e
Adnan Wahab <mail@adnanwahab.com>
2014-04-23 20:48:52 -0700

    switch to MIT license, update cp

---
+++ examples/md/map.md
@@ -0,0 +1,2 @@
+Map of historical events throughout time. Scraped from wikipedia. click on a circle
+for deets. 
---
+++ examples/md/swarm.js
@@ -0,0 +1,13 @@
+d3.select('canvas')
+.selectAll('circle')
+.data(d3.range(1e5))
+.enter().append('circle')
+.attr('fill', function () { return "hsl(" + Math.random() * 360 + ",100%, 50%)" })
+.attr('cx', function (d, i) { return d / 1e8 })
+.attr('cy', function (d, i) { return (2e5  -  d) / 20000 })
+.attr('r', function (d, i) { return d  % 1000 })
+.shader({ cx: 'resolution.x / 2. + cos(pos.z + clock * pos.x) * pos.z * 10.;'
+        , cy: 'resolution.y / 2. + sin(pos.z + clock * pos.x) * pos.z * 10.;'
+        , stroke: 'vec4(unpack_color(stroke).xyz * .5 + vec3(mouse.x / resolution.x, mouse.y / resolution.y, 1.), 1.);'
+        , r: 'pos.y + pos.y * max(distance(x, mouse.x) / resolution.x, distance(y, mouse.y) / resolution.y)'
+        })
