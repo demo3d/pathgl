16e61c3e1d9e19cc3c378d75758222e5bb5aef63
Adnan Wahab <mail@adnanwahab.com>
2014-05-09 22:08:19 -0700

    add IMAGES

--- examples/js/map.js
+++ examples/js/map.js
@@ -1,7 +1,5 @@
 var selector = 'svg'
-var size = {width: .85 * innerWidth, height: innerHeight * .9}
-var width = size.width,
-    height = size.height,
+var size = {width: d3.select('canvas').attr('width'), height: innerHeight * .9}
     rotate = [10, -10],
     velocity = [.03, -.001],
     time = Date.now();
@@ -11,10 +9,10 @@ var proj = d3.geo.wagner4().scale(225).translate([size.width / 2 - 75, size.heig
   , path = d3.geo.path().projection(proj)
 
 var svg = d3.select(selector)
-          .attr("width", width)
-          .attr("height", height)
+          .attr(size)
 
-var webgl = d3.select('canvas').attr(size).attr('class', 'no-click')
+
+var webgl = d3.select('canvas').attr('class', 'no-click')
 var p = d3.select('.blurb')
 
 d3.json('data/world-50m.json', draw_world)
@@ -27,14 +25,12 @@ function mouseover(d) {
 function draw_world(err, world) {
   if (err) return
 
-  d3.select('svg')
-//  webgl
+  d3.select('canvas')
   .append("path")
   .datum(topojson.mesh(world, world.objects.countries, function(a, b) { return a == b && a.id !== 10 }))
   .attr({ class: 'world'
         , d: path
-        , fill: 'none'
-        , 'stroke': 'grey'
+        , fill: 'grey'
         })
 }
 
--- examples/md/physics.md
+++ examples/md/physics.md
@@ -1,2 +1,2 @@
-A simple example of a particle demo using physics computed on the gpu.
+A simple particle simulation using physics computed on the gpu.
 You can tweak the inertia and drag parameters with pathgl.uniform('inertia', .2)
