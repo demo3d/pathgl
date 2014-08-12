c1cf6b20caac54e05188b86a9c83e13363246014
Adnan Wahab <mail@adnanwahab.com>
2014-08-12 10:01:38 -0700

    yep

--- src/texture.js
+++ src/texture.js
@@ -127,7 +127,7 @@ function readFrom(ctx) {
 
 function unwrap() {
   var i = (this.width * this.height / this.slots) || 0, uv = new Array(i)
-  while(i--) uv[i] = { x: this.x()(0, i), y: this.y()(0, i) }
+  while(i--) uv[i] = { x: this.x()(0, i * this.slots), y: this.y()(0, i * this.slots) }
   return uv
 }
 
